0e8e282f148bde40e01ff9768fe092024d4c9ed8
joel.carter <joel.carter@accenture.com>
2019-02-19 16:49:35 -0600

    adding working modal with form

--- Angular Landing/src/app/views/home/intro-one/intro-one.component.ts
+++ Angular Landing/src/app/views/home/intro-one/intro-one.component.ts
@@ -1,69 +1,61 @@
 import { Component, OnInit,Inject } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
+// import {BookingComponent,DialogOverviewExampleDialog} from '../../../booking/booking.component';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
-@Component({
-  selector: 'app-intro',
-  templateUrl: './intro-one.component.html',
-  styleUrls: ['./intro-one.component.scss']
-})
-export class IntroOneComponent implements OnInit {
-
-  constructor(private _router: Router,public dialog: MatDialog) { }
-
-  ngOnInit() {
-  }
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      width: '250px',
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
-  bookEvent()  {
-    this._router.navigate(['/booking']);
-    console.log("This is being called")
-  }
-  getNGLanding() {
-    window.open('');
-  }
-}
-
-export interface DialogData {
+export interface DialogData{
   animal: string;
   name: string;
 }
 
+// @Component({
+//   selector:'dialog-overview-example',
+//   templateUrl: './intro-one.component.html',
+//   styleUrls:['./intro-one.component.scss'],
+// })
+// export class DialogOverviewExample {
+
+
+
+
+
+
+
+// ;
+//   }
+
+// }
 @Component({
   selector: 'app-intro',
   templateUrl: './intro-one.component.html',
-  styleUrls: ['./intro-one.component.scss']
+  styleUrls:['./intro-one.component.scss'],
 })
-export class DialogOverviewExample implements OnInit {
-
-  ngOnInit() {
-  }
+export class IntroOneComponent {
 
   animal: string;
   name: string;
 
   constructor(public dialog: MatDialog) {}
 
- 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
 
 }
 
 @Component({
-  selector: 'app-intro',
-  templateUrl: './intro-one.component.html',
-  styleUrls: ['./intro-one.component.scss']
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './dialog-overview-example.html',
 })
-export class DialogOverviewExampleDialog implements OnInit {
-
-  ngOnInit() {
-  }
+export class DialogOverviewExampleDialog {
 
   constructor(
     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
@@ -74,14 +66,3 @@ export class DialogOverviewExampleDialog implements OnInit {
   }
 
 }
-
-
-// export class DialogOverviewExampleDialog {
-//   constructor (
-//     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-//     @Inject(MAT_DIALOG_DATA) public data: DialogData){}
-
-//     onNoClick(): void {
-//       this.dialogRef.close();
-//     }
-// }
